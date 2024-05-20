b0067c7b42ba6f1b71d2dcb1937732a4d3bc3dc7
Keasanda <98611401+Keasanda@users.noreply.github.com>
2024-05-20 10:55:44 +0200

    Add files via upload
    Wena U lava Reop ya mina

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""square from rectangle"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """square"""
+
+    def __init__(self, size):
+        """square
+
+        Args:
+            self: instance
+            size: square size
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
