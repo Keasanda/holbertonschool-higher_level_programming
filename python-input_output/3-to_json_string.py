a8fb7934f8005b8fbf2a75868745483c4abab07d
Keasanda <98611401+Keasanda@users.noreply.github.com>
2024-05-27 08:27:21 +0200

    Delete python-input_output directory

--- python-input_output/3-to_json_string.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""define function to_json_string"""
-
-import json
-
-
-def to_json_string(my_obj):
-    """return JSON representation of an object"""
-    return json.dumps(my_obj)
